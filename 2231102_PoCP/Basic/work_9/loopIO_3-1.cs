9c85c9382d36bc3fb92834f558cfa85e6582565f
ZiberX <ziberx01@gmail.com>
2024-09-17 19:49:24 +0700

    fix

--- 2231102_PoCP/Basic/work_9/loopIO_3-1.cs
+++ 2231102_PoCP/Basic/work_9/loopIO_3-1.cs
@@ -1,28 +1,37 @@
 Console.Write("Christmas bush : ");
 int number = int.Parse(Console.ReadLine());
-int round = 0;
-// พุ่มไม้
-for (int i = 1; i <= number; i++)
+
+if (number > 2)
 {
-    Console.SetCursorPosition(Console.WindowWidth / 2 - number + 1, Console.CursorTop);
-    for (int j = 1; j <= number; j++)
+    int round = 0;
+    // พุ่มไม้
+    for (int i = 1; i <= number; i++)
     {
-        Console.SetCursorPosition(Console.WindowWidth / 2 - i - j, Console.CursorTop);
-        for (int k = 1; k <= j + round; k++)
+        Console.SetCursorPosition(Console.WindowWidth / 2, Console.CursorTop);
+        for (int j = 1; j <= number; j++)
         {
-            Console.Write(k + " ");
+            Console.SetCursorPosition(Console.WindowWidth / 2 - i - j, Console.CursorTop);
+            for (int k = 1; k <= j + round; k++)
+            {
+                Console.Write(k + " ");
+            }
+            Console.WriteLine();
+        }
+        round++;
+    }
+    // ลำต้น
+    for (int i = 1; i <= number; i++)
+    {
+        Console.SetCursorPosition(Console.WindowWidth / 2 - number + 1, Console.CursorTop);
+        for (int j = 1; j <= number - 2; j++)
+        {
+            Console.Write(j + " ");
         }
         Console.WriteLine();
     }
-    round++;
 }
-// ลำต้น
-for (int i = 1; i <= number; i++)
+else
 {
-    Console.SetCursorPosition(Console.WindowWidth / 2 - number + 1, Console.CursorTop);
-    for (int j = 1; j <= number - 2; j++)
-    {
-        Console.Write(j + " ");
-    }
-    Console.WriteLine();
+    Console.SetCursorPosition(Console.WindowWidth / 2, Console.CursorTop);
+    Console.WriteLine("Error Christmas bush!!!");
 }
