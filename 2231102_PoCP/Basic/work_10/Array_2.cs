a94393afd60e7d2cdc0d5ecbbb9b3e489a0c9bcb
ZiberX <ziberx01@gmail.com>
2024-09-19 22:38:19 +0700

    Update Array_2.cs

--- 2231102_PoCP/Basic/work_10/Array_2.cs
+++ 2231102_PoCP/Basic/work_10/Array_2.cs
@@ -1,16 +1,19 @@
 string[] names = { "TONSON", "CHIDAPA", "NARI", "CHOMPOO", "ARKOM", "NINA", "CARTOON" };
 
 Console.Write("First character to find : ");
-char fName = char.Parse(Console.ReadLine());
+char fName = char.Parse(Console.ReadLine()); // รับค่าตัวอักษร '..' ex. 'c' , 'A'
 
 string strNames = "";
 string strfNames = "";
 foreach (string name in names)
 {
-    strNames += name + " ";
+    strNames += name + " "; // เพิ่ม name (ex. "TONSON", "NARI") เข้าไปในตัวแปรและเว้นวรรค
+
+    // name[0] -> ตัวอักษรตำแหน่งที่ 0 ของตัวแปร name (เป็น char ไม่ใช่ string) | ex. "TONSON" ตัวที่ 0 คือ 'T'
+    // char.ToUpper(ตัวแปร) -> แปลงค่า ตัวอักษรเป็นตัวพิมพ์ใหญ่ ex. 'c' --> 'C'
     if (name[0] == char.ToUpper(fName))
     {
-        strfNames += name + " ";
+        strfNames += name + " "; // เพิ่ม name ที่ตรงกับเงื่อนไข เข้าไปในตัวแปรและเว้นวรรค
     }
 }
 Console.WriteLine("\nOutput :");
